08b102e754869a7346b60f098d1e34e4d01f5c76
genesisarequipa1170-debug <genesis.arequipa1170@utc.edu.ec>
2026-06-06 02:06:24 -0500

    genesis13

---
+++ src/grupo01/Ejercicio07.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo01;
+
+/**
+ *
+ * @author COMPU
+ */
+public class Ejercicio07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("========================================================================================================");
+        System.out.println("Algoritmo que solicite un número entero positivo y genere su tabla de multiplicar desde el 1 hasta el 20.");
+        System.out.println("======================================================================================================");
+        
+    }
+    
+}
