c778d8d5be2ac50b216a2f7e21e07edf00b3bb6c
genesisarequipa1170-debug <genesis.arequipa1170@utc.edu.ec>
2026-06-06 01:52:18 -0500

    genesis7

---
+++ src/grupo01/Ejercicio06.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo01;
+
+import java.util.Scanner;
+/**
+ *
+ * @author COMPU
+ */
+public class Ejercicio06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        int pares = 0;
+        int impares = 0;
+    }
+    
+}
