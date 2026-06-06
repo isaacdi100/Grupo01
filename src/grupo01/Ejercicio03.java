5656b6ac3a07454b4aed62d6c8b4fc3faa39b3e3
isaacdi100 <dilan.analuisa5190@utc.edu.ec>
2026-06-06 01:11:03 -0600

    Isaac 13

---
+++ src/grupo01/Ejercicio03.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    Scanner teclado = new Scanner(System.in);
+        double nota;
+        
+        System.out.println("=== REGISTRO CONTINUO DE NOTAS ===");
+        System.out.println("Ingresa notas entre 0 y 10.");
+        System.out.println("Para terminar todo el proceso, ingresa un número negativo.\n");
+       
+    }
+}
+     
