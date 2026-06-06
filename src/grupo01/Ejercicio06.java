e67ba61d8db53e62bcfa8b4bc2bb0ef0283d5ff7
genesisarequipa1170-debug <genesis.arequipa1170@utc.edu.ec>
2026-06-06 01:53:50 -0500

    genesis8

--- src/grupo01/Ejercicio06.java
+++ src/grupo01/Ejercicio06.java
@@ -17,10 +17,20 @@ public class Ejercicio06 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-
+        //Declarar variables
         int numero;
         int pares = 0;
         int impares = 0;
+        System.out.print("Ingrese un número (0 para terminar): ");
+        numero = sc.nextInt();
+
+        while (numero != 0) {
+
+            if (numero % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
     }
     
 }
