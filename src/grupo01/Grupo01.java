04a19e56e22aedb6f00acc5b028ff98b123eb546
isaacdi100 <dilan.analuisa5190@utc.edu.ec>
2026-06-06 00:17:49 -0600

    Isaac 1

--- src/grupo01/Grupo01.java
+++ src/grupo01/Grupo01.java
@@ -4,6 +4,8 @@
  */
 package grupo01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP
@@ -13,11 +15,19 @@ public class Grupo01 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Yo");
-        System.out.println("yo genesis");
-        System.out.println("POZOLE");
-    }
     
+  public static void credenciales() {
+        // Crear un algoritmo para adivinar credenciales (Intentos ilimitados)
+        Scanner sc = new Scanner(System.in);
+        final String clave = "1234Pozole"; // Contraseña segura
+        
+        int intentos = 0;
+
+      
+    }
+
+    public static void main(String[] args) {
+        // Llamamos a tu método para probarlo
+        credenciales();
+    }
 }
