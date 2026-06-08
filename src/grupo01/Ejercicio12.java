9238ceb97ae8a8a1f4060fc772b2aabf3b5baab1
henryalmachi2491-cpu <henry.almachi2491@utc.edu.ec>
2026-06-07 23:44:26 -0500

    commit23
    manuel

--- src/grupo01/Ejercicio12.java
+++ src/grupo01/Ejercicio12.java
@@ -18,7 +18,19 @@ public class Ejercicio12 {
          Scanner sc = new Scanner(System.in); // Para leer los datos
          final int CANTIDAD = 15; // Cantidad de calificaciones a ingresar
          double suma = 0, promedio, calificacion;
-          int contEncima = 0; // Cuenta cuántas son mayores al promedio
+         int contEncima = 0; // Cuenta cuántas son mayores al promedio
+          // Primero guardamos las calificaciones
+         double[] calificaciones = new double[CANTIDAD];
+         System.out.println("Ingrese " + CANTIDAD + " calificaciones:");
+            // Leer y acumular las calificaciones
+           for (int i = 0; i < CANTIDAD; i++) {
+               System.out.print("Calificación " + (i+1) + ": ");
+               calificaciones[i] = sc.nextDouble();
+                suma += calificaciones[i];
+            }
+           
+           }
+          
     }
     
-}
+
