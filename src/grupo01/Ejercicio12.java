81161aa8e6a121cc81a8b392010addc6fb4ffecc
henryalmachi2491-cpu <henry.almachi2491@utc.edu.ec>
2026-06-07 23:30:18 -0500

    commit21
    henry

--- src/grupo01/Ejercicio12.java
+++ src/grupo01/Ejercicio12.java
@@ -4,10 +4,18 @@
  */
 package grupo01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author kerly
  */
 public class Ejercicio12 {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[]args){
+         Scanner sc = new Scanner(System.in); // Para leer los datos
+    }
     
 }
