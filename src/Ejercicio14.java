82590cdd7496cacefb32c615ff779796aabc3302
alexachote18 <alexachote13@gmail.com>
2026-06-08 01:27:06 -0500

    comit 13

--- src/Ejercicio14.java
+++ src/Ejercicio14.java
@@ -15,13 +15,19 @@ public class Ejercicio14 {
         int suma = 0;
         int inicio;
         
-        System.out.println("Ingrese el numero de inicio(1-200):");
+        System.out.println("Ingrese el numero de inicio :");
         inicio = scanner.nextInt();
         // Si innicio es menor a 1, cambia automaticamente a 1
         if (inicio<1){
             System.out.println("Inicio menor a 1 se cambiara a 1 ");
             inicio = 1 ;
         }
+        int fin ;
+        System.out.println("Ingrese el numero de fin :");
+        fin = scanner.nextInt();
+        if(fin>200){
+            System.out.println("Fin menor a 200 se finalazara en 200");
+            fin=200;
         
         // Ciclo desde 2 hasta 200, solo numeros pares (paso = 2)
         for (int i = 2; i <= 200; i += 2) {
