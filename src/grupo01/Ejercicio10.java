b8f1858869d64ed04c45629dd280ed6e32f5557b
henryalmachi2491-cpu <henry.almachi2491@utc.edu.ec>
2026-06-07 21:51:13 -0500

    comit 8
    bb

--- src/grupo01/Ejercicio10.java
+++ src/grupo01/Ejercicio10.java
@@ -13,7 +13,14 @@ public class Ejercicio10 {
      * @param args the command line arguments
      */
     public static void main(String[]args){
-        Scaner sc =new Scaner(System.in);//para leer datos 
+        Scaner sc =new Scaner(System.in);//para leer datos
+        //contador para cada tipo de numero
+        int contPositivos = 0;
+        int contNegativos = 0;
+        int contCeros = 0;
+        int numero;
+        System.out.println("ingrese los numeros: ");
+        
     }
     
 }
