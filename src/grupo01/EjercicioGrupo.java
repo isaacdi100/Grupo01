242a58d352dc1392e27fff85fcdbca5ef88e4b28
genesisarequipa1170-debug <genesis.arequipa1170@utc.edu.ec>
2026-06-08 21:01:02 -0500

    g

--- src/grupo01/EjercicioGrupo.java
+++ src/grupo01/EjercicioGrupo.java
@@ -56,7 +56,7 @@ public class EjercicioGrupo {
         
         System.out.print("Ingresa un numero: ");
         numero = sc.nextInt();
-
+    //While True
         while (numero >= 0) {
             cantidadNumeros = cantidadNumeros + 1;
             sumaTotal = sumaTotal + numero;       
